bb7b25ca501a7e8370bbdf184fb3304f13c706e2
truongvd05 <truongvdf8@fullstack.edu.vn>
2026-01-20 10:52:53 +0700

    add github page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import { NavLink } from 'react-router'
 import AppRoutes from './components/Approutes'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
       <AppRoutes/>
